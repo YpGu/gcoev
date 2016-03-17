a48a86f252e76b0b7842b1d269332b30fbe5b4c5
YpGu <gyp7364@gmail.com>
2016-03-17 03:03:15 -0400

    1

--- src/lfpm/Main.java
+++ src/lfpm/Main.java
@@ -14,11 +14,21 @@ public class Main {
   public static List<Integer> NS = new ArrayList<Integer>(T);
   public static List<double[][]> GS = new ArrayList<double[][]>(T);
   public static List<double[][]> AS = new ArrayList<double[][]>(T);
+  public static List<double[][]> HS = new ArrayList<double[][]>(T);	      // h
+  public static List<double[][]> HPrimeS = new ArrayList<double[][]>(T);      // h'
+  public static List<double[][]> HHatS = new ArrayList<double[][]>(T);	      // \hat{h}
+  public static List<double[][]> HHatPrimeS = new ArrayList<double[][]>(T);   // \hat{h}'
+
+  /* intrinsic features */
   public static List<double[][]> muS = new ArrayList<double[][]>(T);    // forward
+  public static List<double[][]> VS = new ArrayList<double[][]>(T);	// V^{(t)} 
+
+  /* impression features */
   public static List<double[][]> muHatS = new ArrayList<double[][]>(T); // backward
-  public static List<double[][]> HS = new ArrayList<double[][]>(T);
-  public static List<double[][]> HPrimeS = new ArrayList<double[][]>(T);
+  public static List<double[][]> VHatS = new ArrayList<double[][]>(T);	// \hat{V}^{(t)} 
+
   public static List<Map<Integer, Integer>> idMapS = new ArrayList<Map<Integer, Integer>>(T);
+  public static List<Double> deltaS = new ArrayList<Double>(T);
 
   public static void test1() {
     // read, init data & parameters
@@ -37,16 +47,28 @@ public class Main {
       double[][] muHat = new double[n][1];
       double[][] H = new double[n][1];
       double[][] HPrime = new double[n][1];
+      double[][] h_hat = new double[n][1];
       FileParser.readCSVGraph(fileDir, freq, idMap, G, A);
       for (int i = 0; i < n; i++) {
 	mu[i][0] = scale * (rand.nextDouble() - 0.5);
 	muHat[i][0] = scale * (rand.nextDouble() - 0.5);
 	H[i][0] = scale * (rand.nextDouble() - 0.5);
 	HPrime[i][0] = scale * (rand.nextDouble() - 0.5);
+	h_hat[i][0] = scale * (rand.nextDouble() - 0.5);
       }
 
       GS.add(G); AS.add(A);
       muS.add(mu); muHatS.add(mu); HS.add(H); HPrimeS.add(HPrime);
+      // for test
+      deltaS.add(0.1);
+      double[][] V = new double[n][n];
+      double[][] VHat = new double[n][n];
+      for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) {
+	V[i][j] = 0.01 * scale * (rand.nextDouble() - 0.5);
+	VHat[i][j] = 0.01 * scale * (rand.nextDouble() - 0.5);
+      }
+      VS.add(V); HHatS.add(h_hat);
+      VHatS.add(VHat); 
 
       System.out.println("done! t = " + t);
     }
@@ -56,7 +78,10 @@ public class Main {
   }
 
   /* 
-   * forward pass: update mu (muS)
+   * forward pass 1: update 
+   *  (1) mean mu (muS) 
+   * and 
+   *  (2) variance V (VS)
    */
   public static void forward1() {
     for (int t1 = t0; t1 < T; t1++) {
@@ -64,20 +89,33 @@ public class Main {
       System.out.println("forward\tt = " + t1);
       if (t != 0) {
 	int old_n = NS.get(t-1);	      // N_{t-1}
+	double delta_t = deltaS.get(t);	      // delta_t
 	Matrix a = new Matrix(AS.get(t-1));   // A^{t-1}
 	Matrix mu_pre_t = new Matrix(muS.get(t-1));   // mu^{t-1}
-	Matrix pt = (a.times(a.transpose()).plus(Matrix.identity(old_n, old_n))).times(sigma*sigma);  // P^{t-1}
-	Matrix kt = pt.times(pt.plus(Matrix.identity(old_n, old_n).times(delta*delta)).inverse());    // K^{t}
-	Matrix h_pre_t = Operations.translate(new Matrix(HS.get(t)), idMapS.get(t), idMapS.get(t-1)); // h^{t} (in style t-1)
-	Matrix mu_t = a.times(mu_pre_t).plus(kt.times(h_pre_t.minus(a.times(mu_pre_t))));   // mu^{t} (in style t-1)
-	mu_t = Operations.translate(mu_t, idMapS.get(t-1), idMapS.get(t));    // mu^{t} (in style t)
+	Matrix V_pre_t = new Matrix(VS.get(t-1));     // V^{t-1}
+	Matrix h_hat_t = Operations.translate(new Matrix(HHatS.get(t)), idMapS.get(t), idMapS.get(t-1), false);    // \hat{h}^t  [t-1]
+	Matrix hprime_pre_t = new Matrix(HPrimeS.get(t-1));   // h'^{t-1} 
 
-	muS.set(t, mu_t.getArray());	      // update
+	/* calculate \mu */
+	Matrix wsum = mu_pre_t.times(1-lambda).plus(a.times(hprime_pre_t).times(lambda));
+	Matrix iplusv = Matrix.identity(old_n, old_n).times(delta*delta).plus(V_pre_t.times((1-lambda)*(1-lambda)));
+	Matrix Sigma = (iplusv.inverse().plus(Matrix.identity(old_n, old_n).times(1.0/delta_t/delta_t))).inverse();   // \Sigma  [t-1]
+	Matrix addi = Sigma.times(h_hat_t.minus(wsum)).times(1.0/delta_t/delta_t);
+	Matrix mu_t = Operations.translate(wsum.plus(addi), idMapS.get(t-1), idMapS.get(t), true);	  // mu^t  [t]
+	/* calculate V */
+	Matrix V_t = Operations.translate(Sigma, idMapS.get(t-1), idMapS.get(t), false);   // V^t  [t]
+
+	/* update */
+	muS.set(t, mu_t.getArray());
+	VS.set(t, V_t.getArray());
+
+	/*
 	for (int i = 0; i < mu_t.getRowDimension(); i++) {
 	  System.out.printf("%f ", mu_t.get(i,0));
 	}
 	System.out.print("\n");
-//	System.out.println(mu_t.getRowDimension());
+	*/
+	System.out.println(mu_t.getRowDimension());
       } else {
 	// random init: keep unchanged
       }
@@ -86,30 +124,52 @@ public class Main {
     }
   }
 
-  /*
-   * backward pass: update \hat{mu} (muHatS)
+  /* 
+   * backward pass 1: update 
+   *  (1) mean \hat{mu} (muHatS) 
+   * and 
+   *  (2) variance \hat{V} (VHatS)
    */
   public static void backward1() {
     for (int t1 = T-1; t1 >= t0; t1--) {
       int t = t1-t0;
       System.out.println("backward\tt = " + t1);
+      // TODO: check init condition (because we use t-1 now)
       if (t != T-1-t0) {
-	int n = NS.get(t);    // N_{t}
-	Matrix a = new Matrix(AS.get(t));	// A^{t}
-	Matrix jt = a.transpose().times((Matrix.identity(n, n).plus(a.times(a.transpose()))).inverse());    // J^{t}
-	Matrix mu_t = new Matrix(muS.get(t));	// mu^{t}
-	Matrix mu_next_t = Operations.translate(new Matrix(muHatS.get(t+1)), idMapS.get(t+1), idMapS.get(t));	// \hat{mu}^{t+1}
-	Matrix mu_hat_t = mu_t.plus(jt.times(mu_next_t.minus(a.times(mu_t))));	  // \hat{mu}^{t}
+	int n_pre_t = NS.get(t-1);    // N_{t-1}
+	double rat = (1.0-lambda)/sigma/sigma;
+	Matrix V_pre_t = new Matrix(VS.get(t-1));	// V^{t-1}
+//	Matrix K_pre_t = (V_pre_t.inverse().plus(Matrix.identity(n_pre_t, n_pre_t).times(rat*rat))).inverse();	// K^{t-1}
+	System.out.printf("%d %d\n", V_pre_t.getRowDimension(), V_pre_t.getColumnDimension());
+	Matrix gyp = V_pre_t.inverse();
+	Matrix K_pre_t = (V_pre_t.inverse().plus(Matrix.identity(n_pre_t, n_pre_t).times(rat*rat)));
+	Matrix kkkk = K_pre_t.inverse();
 
-	muHatS.set(t, mu_hat_t.getArray());	// update
-//	System.out.println(mu_hat_t.getRowDimension());
+	Matrix A_pre_t  = new Matrix(AS.get(t-1));	// A^{t-1}
+	Matrix hprime_pre_t = new Matrix(HPrimeS.get(t-1));   // h'^{t-1} 
+	Matrix mu_pre_t = new Matrix(muS.get(t-1));	// \mu^{t-1}
+	Matrix mu_hat_t = Operations.translate(new Matrix(muHatS.get(t)), idMapS.get(t), idMapS.get(t-1), false);   // \hat{\mu}^{t}  [t-1]
+
+	/* calculate \hat{\mu}^{t-1} */
+	Matrix add1 = mu_hat_t.minus(A_pre_t.times(hprime_pre_t));
+	Matrix add2 = V_pre_t.inverse().times(mu_pre_t).times(sigma*sigma/(1-lambda));
+	Matrix mu_hat_pre_t = K_pre_t.times(add1.plus(add2)).times((1-lambda)/sigma/sigma);
+	/* calculate \hat{V}^{t-1} */
+	Matrix V_hat_t = Operations.translate(new Matrix(VHatS.get(t)), idMapS.get(t), idMapS.get(t-1), false);	    // \hat{V}^{t}  [t-1]
+	Matrix kvkt = K_pre_t.times(V_hat_t).times(K_pre_t.transpose());
+	Matrix V_hat_pre_t = K_pre_t.plus(kvkt.times(rat*rat));
+
+	/* update */
+	muHatS.set(t-1, mu_hat_pre_t.getArray());
+	VHatS.set(t-1, V_hat_pre_t.getArray());
+	System.out.println(mu_hat_t.getRowDimension());
       } else {
 	// initial condition for backward pass: \hat{mu}^{T} = mu^{T}
 	Matrix mu_hat_t = new Matrix(muS.get(t));
 	muHatS.set(t, mu_hat_t.getArray());	// update
       }
       Scanner sc = new Scanner(System.in);
-      int gu; gu = sc.nextInt();
+//      int gu; gu = sc.nextInt();
     }
   }
 
--- src/lfpm/Operations.java
+++ src/lfpm/Operations.java
@@ -2,23 +2,61 @@ import java.util.*;
 import Jama.*;
 
 public class Operations {
+  /* 
+   * translate
+   * copy array/matrix according to the id of time <t>
+   * option: 
+   *	true if use random prior
+   *	false if use zero prior
+   */
   public static Matrix 
-  translate(Matrix arr1, Map<Integer, Integer> map1, Map<Integer, Integer> map2) {
+  translate(Matrix arr1, Map<Integer, Integer> map1, Map<Integer, Integer> map2, boolean option) {
     int n2 = map2.size();
-    double[][] arr2 = new double[n2][1];
-    // random prior
-    for (int i = 0; i < n2; i++) {
-      arr2[i][0] = Main.scale * (Main.rand.nextDouble() - 0.5);
-    }
-    // override
-    for (Map.Entry<Integer, Integer> e: map1.entrySet()) {
-      int global_id = e.getKey();
-      if (map2.containsKey(global_id)) {
-	int local_id2 = map2.get(global_id);
-	int local_id1 = map1.get(global_id);
-	arr2[local_id2][0] = arr1.get(local_id1, 0);
+    if (arr1.getColumnDimension() == arr1.getRowDimension()) {
+      /* arr1 is a square matrix */
+      double[][] arr2 = new double[n2][n2];
+      if (option) {
+	/* random prior */ // TODO: what about covariance matrix?
+	for (int i = 0; i < n2; i++) for (int j = 0; j < n2; j++) {
+	  arr2[i][j] = Main.scale * (Main.rand.nextDouble() - 0.5);
+	}
       }
+      /* override some elements */
+      for (Map.Entry<Integer, Integer> e: map1.entrySet()) {
+	int global_id_x = e.getKey();
+	if (map2.containsKey(global_id_x)) {
+	  int local_id2_x = map2.get(global_id_x);
+	  int local_id1_x = map1.get(global_id_x);
+	  for (Map.Entry<Integer, Integer> f: map1.entrySet()) {
+	    int global_id_y = f.getKey();
+	    if (map2.containsKey(global_id_y)) {
+	      int local_id2_y = map2.get(global_id_y);
+	      int local_id1_y = map1.get(global_id_y);
+	      arr2[local_id2_x][local_id2_y] = arr1.get(local_id1_x, local_id1_y);
+	    }
+	  }
+	}
+      }
+      return new Matrix(arr2);
+    } else {
+      /* arr1 is an array */
+      double[][] arr2 = new double[n2][1];
+      if (option) {
+	/* random prior */
+	for (int i = 0; i < n2; i++) {
+	  arr2[i][0] = Main.scale * (Main.rand.nextDouble() - 0.5);
+	}
+      }
+      /* override some elements */
+      for (Map.Entry<Integer, Integer> e: map1.entrySet()) {
+	int global_id = e.getKey();
+	if (map2.containsKey(global_id)) {
+	  int local_id2 = map2.get(global_id);
+	  int local_id1 = map1.get(global_id);
+	  arr2[local_id2][0] = arr1.get(local_id1, 0);
+	}
+      }
+      return new Matrix(arr2);
     }
-    return new Matrix(arr2);
   }
 }
